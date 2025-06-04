455b64b46add0f991cf0a1e4717d531d95252156
aymen bouzerkoune <aymen-bzrk@outlook.com>
2025-06-04 19:32:18 +0200

    replaced special symbols

--- data_analyzer.py
+++ data_analyzer.py
@@ -10,7 +10,7 @@ def scan_port(ip, port, verbose=False):
         return port, True
     else:
         if verbose:
-            print(f'{ip}:{port} - FermÃ©')
+            print(f'{ip}:{port} - Fermé')
         return port, False
 
 def scan_ports(ip, ports, verbose=False):
